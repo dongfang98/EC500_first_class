70a17a72527bd6659bb91f29003eb962cc657e8c
dongfang98 <78338843+dongfang98@users.noreply.github.com>
2021-02-01 15:01:08 +0800

    Update example.py

--- example.py
+++ example.py
@@ -12,11 +12,11 @@ def numpy_add(a,b):
 def numpy_subtract(a,b):
   return np.subtract(a,b)
 
-def test_add():
-  assert add(2,3) == 5
-  assert add('boston','university') == 'bostonuniversity'
-  assert numpy_add(2,3) == 5
+#def test_add():
+#  assert add(2,3) == 5
+#  assert add('boston','university') == 'bostonuniversity'
+#  assert numpy_add(2,3) == 5
   
-def test_subtract():
-  assert subtract(2,3) == -1
-  assert numpy_subtract(2,3) == -1
+#def test_subtract():
+#  assert subtract(2,3) == -1
+#  assert numpy_subtract(2,3) == -1
